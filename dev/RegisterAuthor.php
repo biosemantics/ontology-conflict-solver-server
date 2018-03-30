5713760a69b69426706f942e14afd90cf8c893b8
ermangurses <egurses@email.arizona.edu>
2018-03-29 20:49:02 -0700

    Adding login and register php files for Author and Expert tables

---
+++ dev/RegisterAuthor.php
@@ -0,0 +1,45 @@
+<?php
+    require_once '../includes/DataBaseOperations.php';
+
+    $response = array();
+
+	if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+
+		if( isset($_POST['username']) and isset($_POST['password']) and isset($_POST['firstname'])  and isset($_POST['lastname']) and isset($_POST['email']) ){
+           // operate the data furter
+	       $db = new DataBaseOperations();
+
+		   $result = $db->createAuthor($_POST['username'], $_POST['password'], $_POST['firstname'], $_POST['lastname'], $_POST['email']);
+
+	       if($result == 1 ){
+
+	           $response['error'] = false;
+               $response['message'] = "The user was registered succesfully!";
+
+	       } else if($result == 2 ) {
+
+	       	   $response['error'] = true;
+               $response['message'] = "An error has occurred, please try again!";
+
+	       } else if($result == 0){
+
+               $response['error'] = true; 
+			   $response['message'] = "The username or email are already in use, please choose a different email or username";	
+	       }
+               
+		} else {
+
+            $response['error'] = true;
+            $response['message'] = "The required fields are missing";
+	    }
+	       
+
+	} else {
+		$response['error'] = true;
+        $response['message'] = "Invalid Request";
+	}
+
+	echo json_encode($response);
+
+?>	
