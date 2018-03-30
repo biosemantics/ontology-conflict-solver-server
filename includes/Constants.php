66ea9ab8c18c913dcc1f5b1519393611d37473d1
ermangurses <egurses@email.arizona.edu>
2018-03-29 20:50:50 -0700

    Adding database operations files in php

---
+++ includes/Constants.php
@@ -0,0 +1,6 @@
+<?php
+	define('DB_HOST','localhost');
+	define('DB_NAME','conflict_solver');
+	define('DB_USER_NAME','root');
+	define('DB_PASSWORD','');
+?>
---
+++ includes/DataBaseOperations.php
@@ -0,0 +1,123 @@
+<?php
+    class DataBaseOperations{
+
+    	private $con;
+
+    	function __construct(){
+
+    		require_once dirname(__FILE__). '/DataBaseConnect.php';
+    		$db = new DataBaseConnect();
+    		$this->con = $db->connect();
+    	}
+
+        /****************************************************************************
+        *
+        * Functions for Author Table
+        *
+        * 1) createAuthor
+        * 2) authorLogin
+        * 3) isAuthorExist
+        * 4) getAuthorByUsername
+        *
+        *****************************************************************************/
+
+        public function createAuthor($username, $pass, $firstname, $lastname, $email){
+            
+            if($this->isAuthorExist($username,$email)){
+
+                return 0;
+
+            }else{
+                $password = md5($pass);
+                $stmt = $this->con->prepare("INSERT INTO `Author` (`authorId`,`username`,`password`,`firstname`,`lastname`,`email`) VALUES (NULL, ?, ?, ?, ?, ?);");
+                $stmt->bind_param("sssss",$username,$password,$firstname,$lastname,$email);
+            
+                if($stmt->execute()){
+                    return 1;
+                }else{
+                    return 2;
+                }
+            }
+        }
+
+        public function authorLogin($username, $pass){
+            $password = md5($pass);
+            $stmt = $this->con->prepare("SELECT authorId FROM Author WHERE username = ? AND password = ?");
+            $stmt->bind_param("ss",$username,$password);
+            $stmt->execute();
+            $stmt->store_result(); 
+            return $stmt->num_rows > 0; 
+        }
+
+        private function isAuthorExist($username, $email){
+            $stmt = $this->con->prepare("SELECT authorId FROM Author WHERE username = ? OR email = ?");
+            $stmt->bind_param("ss", $username, $email);
+            $stmt->execute(); 
+            $stmt->store_result(); 
+            return $stmt->num_rows > 0; 
+        }
+        
+        public function getAuthorByUsername($username){
+            $stmt = $this->con->prepare("SELECT * FROM Author WHERE username = ?");
+            $stmt->bind_param("s",$username);
+            $stmt->execute();
+            return $stmt->get_result()->fetch_assoc();
+        }
+
+        /****************************************************************************
+        *
+        * Functions for Expert Table
+        *
+        * 1) createExpert
+        * 2) expertLogin
+        * 3) isExpertExist
+        * 4) getExpertByUsername
+        *
+        *****************************************************************************/
+
+
+    	public function createExpert($username, $pass, $firstname, $lastname, $email){
+            
+            if($this->isExpertExist($username,$email)){
+
+                return 0;
+
+            }else{
+                $password = md5($pass);
+                $stmt = $this->con->prepare("INSERT INTO `Expert` (`expertId`,`username`,`password`,`firstname`,`lastname`,`email`) VALUES (NULL, ?, ?, ?, ?, ?);");
+    		    $stmt->bind_param("sssss",$username,$password,$firstname,$lastname,$email);
+    		
+                if($stmt->execute()){
+    			    return 1;
+    		    }else{
+                    return 2;
+    		    }
+    	    }
+        }
+
+        public function expertLogin($username, $pass){
+            $password = md5($pass);
+            $stmt = $this->con->prepare("SELECT expertId FROM Expert WHERE username = ? AND password = ?");
+            $stmt->bind_param("ss",$username,$password);
+            $stmt->execute();
+            $stmt->store_result(); 
+            return $stmt->num_rows > 0; 
+        }
+
+        private function isExpertExist($username, $email){
+            $stmt = $this->con->prepare("SELECT expertId FROM Expert WHERE username = ? OR email = ?");
+            $stmt->bind_param("ss", $username, $email);
+            $stmt->execute(); 
+            $stmt->store_result(); 
+            return $stmt->num_rows > 0; 
+        }
+        
+        public function getExpertByUsername($username){
+            $stmt = $this->con->prepare("SELECT * FROM Expert WHERE username = ?");
+            $stmt->bind_param("s",$username);
+            $stmt->execute();
+            return $stmt->get_result()->fetch_assoc();
+        }
+
+    }
+?>
