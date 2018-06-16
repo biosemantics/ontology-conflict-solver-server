b50afeb7292c51bd47c7dc8556f5130b008e14ba
ermangurses <egurses@email.arizona.edu>
2018-06-16 10:08:02 -0500

    GetTasks works without problem

--- dev/DailyOperations/GetTasks.php
+++ dev/DailyOperations/GetTasks.php
@@ -3,24 +3,24 @@
 require_once '../../includes/DataBaseOperations.php';
 
 $response = array(); 
+$err = array();
+if($_SERVER['REQUEST_METHOD'] == 'GET'){
 
-	if($_SERVER['REQUEST_METHOD'] == 'GET'){
-
-		$db = new DataBaseOperations();
-	    $result = $db->getTasks();
+	$db = new DataBaseOperations();
+    $result = $db->getTasks();
         
-	  	while( $array = $result->fetch_row() ) {
-	  		$reponse[] = $array;
-            printf ("%s ---> %s\n", $array[0], $array[1]);
-            echo "<br>";
-		}
-
-		echo '<pre>'; print_r($reponse); echo '</pre>';
-
-    } else {
-    	$response['error'] = true;
+  	while( $row = $result->fetch_assoc() ) {
+  		$term = $row['term']; 
+        $username = $row['username'];
+        $data[] = array("term"=>$term, "username"=>$username);
     }
+    $response = $data;
+    $err['error'] = false;
+} else {
+    $err['error'] = true;
+    $err['message'] = "Error happened";
+}
+echo json_encode(array("task_data"=>$response,"error"=>$err));
 
-#echo json_encode($response);
 
 ?>
