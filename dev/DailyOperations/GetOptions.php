fe5443785e00588c280e946d902aba82ca34d084
ermangurses <egurses@email.arizona.edu>
2018-09-01 21:52:33 -0700

    adding dev folder

---
+++ dev/DailyOperations/GetOptions.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once '../../includes/DataBaseOperations.php';
+
+$response = array(); 
+
+	if($_SERVER['REQUEST_METHOD'] == 'GET'){
+
+			$db = new DataBaseOperations();
+			$_GET['term'] = "bristlelike";
+		    $result = $db->getOptions($_GET['term']);
+		    
+		    // output data of each row
+		    	while($row = $result->fetch_assoc()) {
+		    		echo "term: ". $row['term'].  "--->". $row['option_']."<br>";
+		    	}
+    }
+?>
