2413465dde19ab7dd7f0b31d676d1983691a6132
ermangurses <egurses@email.arizona.edu>
2018-09-23 23:56:33 -0700

    sending termId, term, conflictId, username, sentence to the client from the server

--- dev/DailyOperations/GetTasks.php
+++ dev/DailyOperations/GetTasks.php
@@ -1,27 +1,23 @@
 <?php
-
 require_once '../../includes/DataBaseOperations.php';
 
 $response = array(); 
 $err = array();
+    if($_SERVER['REQUEST_METHOD'] == 'GET'){
 
-  if($_SERVER['REQUEST_METHOD'] == 'GET'){
+        $db = new DataBaseOperations();
+        $result = $db->getTasks();
 
-      $db = new DataBaseOperations();
-      $result = $db->getTasks();
-      
-      while( $row = $result->fetch_assoc() ) {
-
-          $termId = $row['termId'];
-          $term = $row['term'];
-          $conflictId = $row['conflictId'];
-          $username = $row['username'];
-
-          $data[] = array("termId"=>$termId, "term"=>$term, "conflictId"=>$conflictId, "username"=>$username);
-      	  //echo "termId: ". $row['termId']. "term: ". $row['term']. "conflictId: ". $row['conflictId']. "username: ". $row['username']."<br>";
-      }
-
-      $response = $data;
-  }
-  echo json_encode(array("task_data"=>$response));
+        while( $row = $result->fetch_assoc() ) {
+            $termId = $row['termId'];
+            $term = $row['term'];
+            $conflictId = $row['conflictId'];
+            $username = $row['username'];
+            $sentence = $row['sentence'];
+            $data[] = array("termId"=>$termId, "term"=>$term, "conflictId"=>$conflictId, "username"=>$username, "sentence"=>$sentence);
+            //echo "termId: ". $row['termId']. "term: ". $row['term']. "conflictId: ". $row['conflictId']. "username: ". $row['username']. "sentence: ". $row['sentence']. "<br>";
+        }
+        $response = $data;
+}
+echo json_encode(array("task_data"=>$response));
 ?>
