8f5504600df2dfded93c2d7044d8c528d7f8eee5
ermangurses <egurses@email.arizona.edu>
2018-09-30 22:29:02 -0700

    adding the picture to array

--- dev/DailyOperations/GetOptions.php
+++ dev/DailyOperations/GetOptions.php
@@ -13,9 +13,11 @@ $response = array();
             $term       = $row['term'];
             $option_    = $row['option_'];
             $definition = $row['definition'];
+            $picture    = $row['picture'];
+           
             //echo "term: ". $row['term']. "  --->  " . $row['option_'].  " ---> ". $row['definition']. "<br>";
             //$data[] = array("term"=>$term, "option_"=>$option_, "definition"=>$definition);
-            $data[] = array("option_"=>$option_, "definition"=>$definition);
+            $data[] = array("option_"=>$option_, "definition"=>$definition, "picture"=>$picture);
 	}
         $response = $data;
     }
