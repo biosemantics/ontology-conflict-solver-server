c724a1926e11df0b116664e6b8a8e2f2c30cd085
ermangurses <egurses@email.arizona.edu>
2018-10-04 23:59:36 -0700

    cleaning

--- dev/DailyOperations/GetOptions.php
+++ dev/DailyOperations/GetOptions.php
@@ -13,11 +13,8 @@ $response = array();
             $term       = $row['term'];
             $option_    = $row['option_'];
             $definition = $row['definition'];
-            $picture    = $row['picture'];
-           
-            //echo "term: ". $row['term']. "  --->  " . $row['option_'].  " ---> ". $row['definition']. "<br>";
+            $picture    = $row['picture'];           
             $data[] = array("term"=>$term, "option_"=>$option_, "definition"=>$definition);
-            //$data[] = array("option_"=>$option_, "definition"=>$definition, "picture"=>$picture);
 	}
         $response = $data;
     }
