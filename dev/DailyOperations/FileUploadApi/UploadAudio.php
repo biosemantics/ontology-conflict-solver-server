ab21f05b1acee69f9c5d4a4f7386e481b806df00
ermangurses <egurses@email.arizona.edu>
2018-10-26 19:03:29 -0700

    comment is taken from the line that gives server response for failed updates

--- dev/DailyOperations/FileUploadApi/UploadAudio.php
+++ dev/DailyOperations/FileUploadApi/UploadAudio.php
@@ -1,8 +1,9 @@
 <?php
+    
     $target_dir = 'Audio/'; //folder name where your files will be stored. create this folder inside "file_upload_api" folder
     $target_file = $target_dir . basename($_FILES["file"]["name"]);
 
-    //$response = array('success' => false, 'message' => 'Sorry, there was an error uploading your file.');
+    $response = array('success' => false, 'message' => 'Sorry, there was an error uploading your file.');
 
     $data = $_POST['sender_information'];
     $json_data = json_decode($data , true);
@@ -11,7 +12,6 @@
     if(move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)){
         $response = array('success' => true, 'file_type' => 'File type is' . $file_type);
     }
-    
     //$response = array('success' => true, 'message' =>$target_dir);
     
     echo json_encode($response);
