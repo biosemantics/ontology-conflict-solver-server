db786dfb7a37e940b7b9fc7ef9d899ad14b619fa
ermangurses <egurses@email.arizona.edu>
2018-11-13 13:09:44 -0700

    The function is added that create connections to the Firebase

--- includes/DataBaseOperations.php
+++ includes/DataBaseOperations.php
@@ -270,6 +270,32 @@
             $stmt->bind_param("ss",$conflictId, $expertId);
             $stmt->execute();
             return $stmt->get_result();
-        }     
+        }
+        public function send_notification($tokens, $message){
+
+            $url = 'https://fcm.googleapis.com/fcm/send';
+            $fields = array('registration_ids' => $tokens,
+                        'data' => $message );
+
+            $headers = array('Authorization:key = AAAAYXS_iEo:APA91bGZ50RhB0sZIBf6vmXohxOd_wJsDVCQPJCMeqtujIfG9JhLPUpA5C4Q_OFW-nacNXHfoSJPjJKMagr54b9i4JUFpcXocf2oAGzrVaTMsKpBNufnNAGRRQrO-CHGJ3eSdjnv9twF',
+                         'Content-Type: application/json');
+            $ch = curl_init();
+
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);  
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+
+            $result = curl_exec($ch);
+            
+            if ($result === FALSE) {
+                die('Curl failed: ' . curl_error($ch));
+            }
+            curl_close($ch);
+            return $result;
+        }       
     }
 ?>
