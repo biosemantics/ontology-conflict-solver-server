37604b10dad9f920adaa5b07ba787fda1a718bc0
ermangurses <egurses@email.arizona.edu>
2018-11-14 12:30:51 -0700

    GetTasks function works fine with control mechanism

--- dev/DailyOperations/GetTasks.php
+++ dev/DailyOperations/GetTasks.php
@@ -3,22 +3,48 @@ require_once '../../includes/DataBaseOperations.php';
 
 $response = array(); 
 $err = array();
-    if($_SERVER['REQUEST_METHOD'] == 'GET'){
+$data1 = array();
+$data2 = array();
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+        $expertId = $_POST["expertId"];
 
         $db = new DataBaseOperations();
-        $result = $db->getTasks();
+        $resultSolved = $db->getTasksSolved($expertId);
+        $resultUnsolved = $db->getTasksUnsolved($expertId);
 
-        while( $row = $result->fetch_assoc() ) {
+        while( $row = $resultSolved->fetch_assoc() ) {
             $termId = $row['termId'];
             $term = $row['term'];
             $conflictId = $row['conflictId'];
             $username = $row['username'];
             $sentence = $row['sentence'];
-            $isSolved = $row['isSolved'];
-            $data[] = array("termId"=>$termId, "term"=>$term, "conflictId"=>$conflictId, "username"=>$username, "sentence"=>$sentence,"isSolved"=>$isSolved);
-            //echo "termId: ". $row['termId']. "term: ". $row['term']. "conflictId: ". $row['conflictId']. "username: ". $row['username']. "sentence: ". $row['sentence']. "<br>";
+            $isSolved = 1;
+            $data1[] = array("termId"=>$termId, "term"=>$term, "conflictId"=>$conflictId, "username"=>$username, "sentence"=>$sentence,"isSolved"=>$isSolved);
         }
-        $response = $data;
+
+        while( $row = $resultUnsolved->fetch_assoc() ) {
+            $termId = $row['termId'];
+            $term = $row['term'];
+            $conflictId = $row['conflictId'];
+            $username = $row['username'];
+            $sentence = $row['sentence'];
+            $isSolved = 0;
+            $data2[] = array("termId"=>$termId, "term"=>$term, "conflictId"=>$conflictId, "username"=>$username, "sentence"=>$sentence,"isSolved"=>$isSolved);
+        }
+
+        $response = array_merge($data1, $data2);
+
+        if( empty($response) ){
+
+            $stat = "Null";
+
+        } else {
+
+            $stat = "NotNull";
+
+        }
+
 }
-echo json_encode(array("task_data"=>$response));
+echo json_encode(array("task_data"=>$response,"status"=>$stat));
 ?>
