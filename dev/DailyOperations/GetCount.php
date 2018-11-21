63afac4db522924e0e9d8f4e62c40ca63ee7ec73
ermangurses <egurses@email.arizona.edu>
2018-11-21 15:55:00 -0700

    Typo fixed

--- dev/DailyOperations/GetCount.php
+++ dev/DailyOperations/GetCount.php
@@ -11,8 +11,8 @@ require_once '../../includes/DataBaseOperations.php';
 	        $result = $db->countUnsolvedConflictsByExpert($expertId);
             
             $count = $result['COUNT(conflictId)'];
-            $unsolvedData[] = array("$count"=>$$count);
-            
+            $unsolvedData[] = array("$count"=>$count);
+
 	        echo json_encode($unsolvedData);		                 
 		}
 	}
