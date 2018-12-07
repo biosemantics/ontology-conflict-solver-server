d31fbd48ab6cd9f025c95730fdde3c44a885eeb6
ermangurses <egurses@email.arizona.edu>
2018-12-07 15:35:55 -0700

    Adding the credentials for DB

--- includes/Constants.php
+++ includes/Constants.php
@@ -1,6 +1,6 @@
 <?php
-	define('DB_HOST','localhost');
-	define('DB_NAME','conflict_solver');
-	define('DB_USER_NAME','root');
-	define('DB_PASSWORD','');
+	define('DB_HOST','127.0.0.1');
+	define('DB_NAME','ontologyconflict');
+	define('DB_USER_NAME','ontologyconflict');
+	define('DB_PASSWORD','uoGXRk83iT8g4Fvss5Lc');
 ?>
